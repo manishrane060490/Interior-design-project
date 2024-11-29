764e603ebac439f09fe9b45ffc62fceeb293a0f5
Manish Rane <manish.rane6490@gmail.com>
2024-11-29 23:13:49 +0530

    added authentication in project with clerk and save data in neon db

---
+++ app/dashboard/page.js
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Dashboard
+
+      <UserButton />
+    </div>
+  )
+}
+
+export default page
---
+++ app/provider.js
@@ -0,0 +1,35 @@
+'use client'
+
+import { useUser } from '@clerk/clerk-react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { UserDetailContext } from './_context/UserDetailContext';
+
+function Provider({children}) {
+  const {user} = useUser();
+  const [userDetail, setUserDetail] = useState();
+
+  useEffect(() => {
+    user && verifyUser()
+  }, [user])
+
+
+  const verifyUser = async () => {
+    const dataResult = await axios.post('/api/verify-user', {
+        user: user
+    })
+
+    console.log(dataResult.data);
+    setUserDetail(dataResult.data);
+  }
+
+  return (
+    <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
+        <div>
+            {children}
+        </div>
+    </UserDetailContext.Provider>
+  )
+}
+
+export default Provider
